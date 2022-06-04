b55084b0739c0ecaf3420658c8b1799a2955ade5
jrgaikwad <gaikwadjeevan73@gmail.com>
2022-06-04 20:33:35 +0530

    Commit from G1 @ 8:33PM

---
+++ src/assertions/HardAssert5.java
@@ -0,0 +1,23 @@
+package assertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HardAssert5 {
+  @Test
+  public void f() {
+	  
+	  //5555555555555555555555555555555555555555555555555555
+	  System.out.println("Assert 5 Started");
+	  String d = "Dinesh";
+	  String c = "Dinfdhgsesh";
+	  String cc = "Dinesh";
+	  Assert.assertEquals(d, "Dinesh");
+	  Assert.assertEquals(2, 3);
+	  System.out.println("After First Assertion");
+	  System.out.println("hi hhh");
+	  System.out.println("hi hhh");
+ 
+  
+  }
+}
