dc91d6e8a1b4f589109bd8f73b87d1d264c70390
jrgaikwad <jeevang0101@gmail.com>
2022-06-04 18:12:43 +0530

    commit4

---
+++ src/assertions/HardAssert4.java
@@ -0,0 +1,19 @@
+package assertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HardAssert4 {
+  @Test
+  public void f() {
+	  System.out.println("Test Started");
+	  String d = "Ganesh";
+	  String c = "ganesh";
+	  Assert.assertEquals(d, "Dinesh");
+	  System.out.println("After First Assertion");
+	  System.out.println("hi hhh");
+	  System.out.println("hi hhh");
+ 
+  
+  }
+}
