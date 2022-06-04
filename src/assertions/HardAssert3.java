a9c86d7f854c300d01c9ea9114afa1c56b4db332
jrgaikwad <gaikwadjeevan73@gmail.com>
2022-06-04 18:01:09 +0530

    Jeevang0101 4th June Task

--- src/assertions/HardAssert3.java
+++ src/assertions/HardAssert3.java
@@ -3,14 +3,13 @@ package assertions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HardAssert1 {
+public class HardAssert3 {
   @Test
   public void f() {
 	  System.out.println("Test Started");
-	  String d = "Dinesh";
-	  String c = "Dinesh";
+	  String d = "Ganesh";
+	  String c = "ganesh";
 	  Assert.assertEquals(d, "Dinesh");
-	  Assert.assertEquals(2, 3);
 	  System.out.println("After First Assertion");
 	  System.out.println("hi hhh");
 	  System.out.println("hi hhh");
